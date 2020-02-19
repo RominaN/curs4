319e6d2c589ca5838b0cf04725eaadd0a06c3418
Romina <44588411+RominaN@users.noreply.github.com>
2020-02-19 21:38:17 +0200

    Add files via upload

---
+++ Imobil.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curs4_2
+{
+    class Imobil
+    {   public string tipCasa { get; set; }
+        public int  nrEtaje { get; set; }
+        public double pret { get; set; }
+
+      
+            public string afiseazaDetaliiImobil()
+            {
+            return "tipCasa: " + tipCasa + "\nNumarEtaj: " + nrEtaje + "\nPret: " +pret;
+            }
+        
+
+        public Imobil()
+        {
+            tipCasa = " ";
+            nrEtaje = 0;
+            pret = 0;
+        }
+
+
+        public Imobil(string type)
+        {
+            this.tipCasa = type;
+            nrEtaje = 0;
+            pret = 0.0;
+        }
+        public Imobil(string tipCasa, int nrEtaje, double pret)
+        {
+            this.tipCasa = tipCasa;
+            this.nrEtaje = nrEtaje;
+            this.pret = pret;
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curs4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Imobil imo = new Imobil();
+            imo.afiseazaDetaliiImobil();
+            imo.tipCasa = "Casa";
+            imo.nrEtaje = 1;
+            imo.pret=25.23;
+
+            Console.WriteLine ("tipCasa: " + imo.tipCasa + "\nNumarEtaje: " + imo.nrEtaje + "\nPret: " + imo.pret);
+
+            Imobil imo2 = new Imobil("apartament");
+            imo2.nrEtaje = 5;
+            imo2.pret = 72.57;
+            Console.WriteLine();
+            Console.WriteLine("tipCasa: " + imo2.tipCasa + "\nNumarEtaj: " + imo2.nrEtaje + "\nPret: " + imo2.pret);
+
+            Imobil imo3 = new Imobil("bloc", 7, 50500);
+            Console.WriteLine();
+            Console.WriteLine("tipCasa: " + imo3.tipCasa + "\nNumarEtaj: " + imo3.nrEtaje + "\nPret: " + imo3.pret);
+
+            Console.ReadLine();
+        }
+    }
+}
